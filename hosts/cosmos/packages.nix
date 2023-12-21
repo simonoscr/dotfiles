94ec00d4e0a02fb984751f97f71536f3012e8e3b
simonoscr <117449098+simonoscr@users.noreply.github.com>
2023-12-21 20:14:38 +0100

    fix: activate steam with dependencies

--- hosts/cosmos/packages.nix
+++ hosts/cosmos/packages.nix
@@ -16,29 +16,29 @@
     corectrl
   ];
 
-  #programs = {
-  #  steam = {
-  #    enable = true;
-  #    package = pkgs.steam.override {
-  #      extraEnv = {};
-  #      extraLibraries = pkgs:
-  #        with pkgs; [
-  #          xorg.libXcursor
-  #          xorg.libXi
-  #          xorg.libXinerama
-  #          xorg.libXScrnSaver
-  #          libpng
-  #          libpulseaudio
-  #          libvorbis
-  #          stdenv.cc.cc.lib
-  #          libkrb5
-  #          keyutils
-  #        ];
-  #    };
-  #  };
-  #  #gamescope = {
-  #  #  enable = true;
-  #  #  capSysNice = true;
-  #  #};
-  #};
+  programs = {
+    steam = {
+      enable = true;
+      package = pkgs.steam.override {
+        extraEnv = {};
+        extraLibraries = pkgs:
+          with pkgs; [
+            xorg.libXcursor
+            xorg.libXi
+            xorg.libXinerama
+            xorg.libXScrnSaver
+            libpng
+            libpulseaudio
+            libvorbis
+            stdenv.cc.cc.lib
+            libkrb5
+            keyutils
+          ];
+      };
+    };
+    #gamescope = {
+    #  enable = true;
+    #  capSysNice = true;
+    #};
+  };
 }
