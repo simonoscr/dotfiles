7b0e94213e08f758cf309e631952e6194ac67497
simonoscr <117449098+simonoscr@users.noreply.github.com>
2023-12-22 23:53:17 +0100

    chore: update flock

--- hosts/cosmos/xserver.nix
+++ hosts/cosmos/xserver.nix
@@ -16,7 +16,13 @@
         default_session = {
           user = "simon";
           #command = "Hyprland";
-          command = "${pkgs.greetd.tuigreet}/bin/tuigreet -g LOGIN -t -r --cmd Hyprland";
+          #command = "${pkgs.greetd.tuigreet}/bin/tuigreet -g LOGIN -t -r --cmd Hyprland";
+          command = ''
+            ${pkgs.greetd.tuigreet}/bin/tuigreet \
+              -g LOGIN -t -r \
+              --sessions ${config.services.xserver.displayManager.sessionData.desktops}/share/wayland-sessions \
+              --xsessions ${config.services.xserver.displayManager.sessionData.desktops}/share/xsessions
+          '';
         };
       };
     };
