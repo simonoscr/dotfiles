e4415fc34b7cba6fe280d0a21a7411435e0be95e
simonoscr <117449098+simonoscr@users.noreply.github.com>
2024-01-19 18:48:24 +0100

    fix: add helm to voyager

--- hosts/voyager/k3s.nix
+++ hosts/voyager/k3s.nix
@@ -6,5 +6,9 @@
   services.k3s.extraFlags = toString [
     # "--kubelet-arg=v=4" # Optionally add additional args to k3s
   ];
-  environment.systemPackages = [ pkgs.k3s ];
+  environment.systemPackages = with pkgs; [
+    k3s
+    kubectl
+    kubernetes-helm
+  ];
 }
