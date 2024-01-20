ace27eb411a20846ce0581acaf75a91119ea476e
simonoscr <117449098+simonoscr@users.noreply.github.com>
2024-01-20 21:01:46 +0100

    fix: switch to new user

--- hosts/voyager/ssh.nix
+++ hosts/voyager/ssh.nix
@@ -4,8 +4,8 @@
   inputs,
   ...
 }: let
-  userhome = config.users.users.host.home;
-  username = config.users.users.host.name;
+  userhome = config.users.users.nixos.home;
+  username = config.users.users.nixos.name;
 in {
   imports = [inputs.sops-nix.nixosModules.sops];
 
