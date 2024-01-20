8a43943c76a25a97abb467e1f7fb49c1414d4ca1
simonoscr <117449098+simonoscr@users.noreply.github.com>
2024-01-20 14:59:53 +0100

    fix: boolean without quotes for sure

--- hosts/voyager/ssh.nix
+++ hosts/voyager/ssh.nix
@@ -11,8 +11,8 @@
     openssh = {
       enable = true;
       settings = {
-        PasswordAuthentication = "false";
-        KbdInteractiveAuthentication = "false";
+        PasswordAuthentication = false;
+        KbdInteractiveAuthentication = false;
         PermitRootLogin = "no";
       };
     };
