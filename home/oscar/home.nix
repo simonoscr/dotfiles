30e23fe939ce06ab5e7fe65193ea768ea0f56af7
simonoscr <117449098+simonoscr@users.noreply.github.com>
2024-01-28 16:15:25 +0100

    fix: update home.manager user for server

--- home/oscar/home.nix
+++ home/oscar/home.nix
@@ -1,10 +1,20 @@
 {pkgs, ...}: {
   imports = [
     ../../modules/git.nix
+    ../../modules/terminals/kitty.nix
+    ../../modules/editors/neovim.nix
+    ../../modules/shell/starship.nix
     ../../modules/shell/zsh.nix
   ];
 
   programs.home-manager.enable = true;
+
+  home = {
+    username = "oscar";
+    sessionVariables = {
+      SHELL = "${pkgs.zsh}/bin/zsh";
+    };
+  };
 
   home.stateVersion = "23.11";
 }
