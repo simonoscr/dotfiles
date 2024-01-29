a79e267fa2d13475d57a423cda665f6f90432b29
simonoscr <117449098+simonoscr@users.noreply.github.com>
2024-01-29 17:49:13 +0100

    fix: update local dns

--- hosts/voyager/network.nix
+++ hosts/voyager/network.nix
@@ -12,27 +12,52 @@
       enable = true;
       allowPing = true;
       logRefusedConnections = false;
-      allowedTCPPorts = [22];
-      # allowedUDPPorts = [ ... ];
+      allowedTCPPorts = [22 53];
+      allowedUDPPorts = [53];
     };
-    dhcpcd.enable = false;
+    #dhcpcd.enable = false;
+    useDHCP = false;
     nameservers = ["127.0.0.1"];
+    defaultGateway = "192.168.178.1";
+    defaultGateway6 = {
+      address = "fe80::1eed:6fff:fef1:8f6f";
+      interface = "wlo1";
+    };
     #extraHosts = ''
     #'';
-  };
-
-  boot.initrd.network = {
-    enable = true;
-    ssh = {
-      enable = true;
-      port = 22;
-      #hostKeys = [
-      #  #FIXME this has to be manually uploaded during installation...
-      #  # scp /tmp/initrd-ssh-key root@95.217.199.121:/mnt/var/lib/initrd-ssh-key
-      #  "/var/lib/initrd-ssh-key"
-      #];
+    interfaces.wlo1 = {
+      ipv4 = {
+        addresses = [
+          {
+            address = "192.168.178.91";
+            prefixLength = 24;
+          }
+        ];
+      };
+      ipv6 = {
+        addresses = [
+          {
+            address = "2a04:4540:641c:2300:b2dc:efff:fe81:46f6";
+            prefixLength = 64;
+          }
+        ];
+      };
     };
   };
+
+  ## FIXME add initrd ssh host key
+  #boot.initrd.network = {
+  #  enable = true;
+  #  ssh = {
+  #    enable = true;
+  #    port = 22;
+  #    #hostKeys = [
+  #    #  #FIXME this has to be manually uploaded during installation...
+  #    #  # scp /tmp/initrd-ssh-key root@95.217.199.121:/mnt/var/lib/initrd-ssh-key
+  #    #  "/var/lib/initrd-ssh-key"
+  #    #];
+  #  };
+  #};
 
   systemd = {
     services = {
--- hosts/voyager/unbound.nix
+++ hosts/voyager/unbound.nix
@@ -1,18 +1,19 @@
 {
   services.unbound = {
     enable = true;
-    settings.server = {
-      prefetch = "yes";
-      prefetch-key = true;
-      qname-minimisation = true;
-      # Too many broken dnssec setups even at big companies such as amazon.
-      # Breaks my email setup. Better rely on tls for security.
-      val-permissive-mode = "yes";
+    settings = {
+      server = {
+        interface = ["::1" "127.0.0.1" "192.168.178.91"];
+        prefetch = "yes";
+        prefetch-key = true;
+        qname-minimisation = true;
+        # Too many broken dnssec setups even at big companies such as amazon.
+        # Breaks my email setup. Better rely on tls for security.
+        val-permissive-mode = "yes";
+      };
+      
     };
   };
-
-  # don't conflict with unbound
-  services.unbound.settings.server.interface = ["::1" "127.0.0.1" "192.168.178.91"];
   # no need for a systemd-resolved stub
   services.resolved.enable = false;
 
