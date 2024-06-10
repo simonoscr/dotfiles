46905f50e19f72fe53bfbddecbce9130b29e6a83
simonoscr <13078509-simonoscr@users.noreply.gitlab.com>
2024-06-10 18:12:03 +0200

    fix: disable gamescope module

--- system/gaming/gamescope.nix
+++ system/gaming/gamescope.nix
@@ -1,15 +1,15 @@
-{config, ...}: {
+_: {
   programs.gamescope = {
-    enable = true;
+    enable = false;
     capSysNice = false; #https://discourse.nixos.org/t/unable-to-activate-gamescope-capsysnice-option/37843
     args = ["--rt"];
   };
 
   # doesn't work at the moment, so caps not set, but doesn't hurt to keep this here
-  security.wrappers.gamescope = {
-    owner = "root";
-    group = "root";
-    source = "${config.programs.gamescope.package}/bin/gamescope";
-    capabilities = "cap_sys_ptrace,cap_sys_nice+pie";
-  };
+  #security.wrappers.gamescope = {
+  #  owner = "root";
+  #  group = "root";
+  #  source = "${config.programs.gamescope.package}/bin/gamescope";
+  #  capabilities = "cap_sys_ptrace,cap_sys_nice+pie";
+  #};
 }
