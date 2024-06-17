ec64082e501a4c235cc50c5e4d841dfb949851f3
simonoscr <13078509-simonoscr@users.noreply.gitlab.com>
2024-06-17 20:40:39 +0200

    fix(fonts): adjust font config and added font config to work profile

--- home/work/services/fonts.nix
+++ home/work/services/fonts.nix
@@ -1,3 +1,36 @@
-_: {
-  fonts.fontconfig.enable = true;
+{ pkgs, ... }:
+{
+  fonts = {
+    fontconfig = {
+      enable = true;
+      defaultFonts = {
+        serif = [
+          "Noto Serif"
+          "Noto Color Emoji"
+        ];
+        sansSerif = [
+          "Inter"
+          "Noto Color Emoji"
+        ];
+        monospace = [
+          "JetBrainsMono Nerd Font"
+          "Noto Color Emoji"
+        ];
+        emoji = [ "Noto Color Emoji" ];
+      };
+      antialias = true;
+      cache32Bit = true;
+      hinting = {
+        enable = true;
+        autohint = true;
+      };
+    };
+  };
+  home.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    (google-fonts.override { fonts = [ "Inter" ]; })
+    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+  ];
 }
--- hosts/desktop/programs/fonts.nix
+++ hosts/desktop/programs/fonts.nix
@@ -2,43 +2,37 @@
 {
   fonts = {
     enableDefaultPackages = false;
+    fontDir.enable = true;
     fontconfig = {
       enable = true;
       defaultFonts = {
         serif = [
-          "Symbols Nerd Font"
           "Noto Serif"
           "Noto Color Emoji"
         ];
         sansSerif = [
-          "Symbols Nerd Font"
           "Inter"
           "Noto Color Emoji"
         ];
         monospace = [
-          "Symbols Nerd Font"
-          "JetBrains Mono"
+          "JetBrainsMono Nerd Font"
           "Noto Color Emoji"
         ];
-        emoji = [
-          "Symbols Nerd Font"
-          "Noto Color Emoji"
-        ];
+        emoji = [ "Noto Color Emoji" ];
       };
       antialias = true;
       cache32Bit = true;
-      hinting.enable = true;
-      hinting.autohint = true;
+      hinting = {
+        enable = true;
+        autohint = true;
+      };
     };
-    fontDir.enable = true;
     packages = with pkgs; [
       noto-fonts
       noto-fonts-cjk
       noto-fonts-emoji
-      jetbrains-mono
-      roboto
       (google-fonts.override { fonts = [ "Inter" ]; })
-      (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
+      (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
     ];
   };
 }
