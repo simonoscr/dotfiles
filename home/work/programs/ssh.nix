07fbc645e6fe24e515f595aaab22794c659f6a92
Simon Siedl <>
2024-06-18 14:22:53 +0200

    fix: adjust work profile with custom configs

--- home/work/programs/ssh.nix
+++ home/work/programs/ssh.nix
@@ -3,10 +3,5 @@
   programs.ssh = {
     enable = true;
     includes = [ "${config.home.homeDirectory}/.ssh/extra_config" ];
-    extraConfig = ''
-      Host gitlab.com
-        User git
-        IdentityFile ${config.home.homeDirectory}/.ssh/id_rsa
-    '';
   };
 }
